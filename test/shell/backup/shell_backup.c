510d60b692c4b64eeab9fb24cc8e6a704fa30aba
Iyanu456 <oyerindei13@gmail.com>
2022-11-13 13:59:25 -0800

    default message

--- test/shell/backup/shell_backup.c
+++ test/shell/backup/shell_backup.c
@@ -9,7 +9,7 @@ int main(int ac, char **av)
 
 	ssize_t linesize = 0;
 
-	int conv_str, count = 0, int_mode = isatty(STDIN);
+	int conv_str, ctn = 0, ctn2 = 0, int_mode = isatty(STDIN);
 
 	while (1)
 	{
@@ -21,27 +21,35 @@ int main(int ac, char **av)
 			}
 
 		linesize = getline(&line, &len, stdin);
-
+		if (line[0] == 'I')
+			printf("true\n");
+		printf("%s, size = %zd\n", line, linesize);
 		buff = create_tokens(line);
-
 		while (*buff != NULL)
 		{
 			tmp_str = *buff;
 
-			while (tmp_str[count] != '\0')
+			while (tmp_str[ctn] != '\0')
 			{
-				if (tmp_str[count] == '\n')
-					tmp_str[count] = '\0';
-				count++;
+				if (tmp_str[ctn] == '\n')
+					tmp_str[ctn] = '\0';
+				ctn++;
 			}
-
-			count = 0;
-
+			ctn = 0;
 			if (_strcmp(tmp_str, "exit") == 0)
 				printf("They are the same\n");
-
+			while (tmp_str[ctn2] != '\0')
+			{
+				printf("%c\n", tmp_str[ctn2]);
+				ctn2++;
+			}
+			ctn2 = 0;
 			buff++;
 		}
+		if (tmp_str == "Iyanu")
+			printf("true, %s\n", buff[0]);
+
 	}
+	exit(98);
 	return (0);
 }
--- test/shell/test.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-int main(void)
-{
-	char **str, line[] = "Iyanu is a boy";
-	str = _tokenizer(line);
-	printf("%s\n", line);
-	return(0);
-}
